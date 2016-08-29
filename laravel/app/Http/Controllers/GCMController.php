a4b2921bb083e0d91942263ee2ea8018ca72a320
Leo Liu <liuyuze1993@qq.com>
2016-08-29 12:29:43 +1200

    cleanup

--- laravel/app/Http/Controllers/GCMController.php
+++ laravel/app/Http/Controllers/GCMController.php
@@ -17,25 +17,32 @@ class GCMController extends Controller
 		// API access key from Google API's Console
 		define('API_ACCESS_KEY', $gcmKey->api_key);//'AIzaSyD-NwwakxSb9czyuRycV6reTBjq0OJqhKE');
 		
-		$registrationIds = $gcmKey->registration_token;
+		$registrationIds = $gcmKey->registration_token;//"cYLIzTmJ598:APA91bFyjjsr5TuGCmBP6CZ6bb2fYhwm21joPsvxmN50873Csjaqc36EB3qSgk63OKcbe3X-QRbf6j_zCpjHk79nuNwwE8B8mblUUTl3CxNBFaKgjK9nWig7PhVmFpYpLVcwFyxd8YoD";
 		
 		//prep the bundle
 		$msg = array
 		(
+		    'isData' => 'false',
 			'message' => 'here is a message. message',
 			'title'	=> 'This is a title. title',
 			'subtitle' => 'This is a subtitle. subtitle',
 			'tickerText' => 'Ticker text here...Ticker text here...Ticker text here',
 			'vibrate' => 1,
 			'sound'	=> 1,
-			'largeIcon'	=> 'large_icon',
-			'smallIcon'	=> 'small_icon'
+			'largeIcon' => 'large_icon',
+			'smallIcon' => 'small_icon'
 		);
+
+        $dataMessage = array (
+            'isData' => 'true',
+            'command' => 'getLocation'
+        );
 		
 		$fields = array
 		(
 			'registration_ids' => array($registrationIds),
-			'data' => array("message" => $msg)
+            'priority' => 'high',
+			'data' => (array) $dataMessage
 		);
 		 
 		$headers = array
@@ -44,7 +51,7 @@ class GCMController extends Controller
 			'Content-Type: application/json'
 			//TODO encryption?
 		);
-		 
+
 		$connection = curl_init();
 		curl_setopt($connection, CURLOPT_URL, 'https://android.googleapis.com/gcm/send');
 		curl_setopt($connection, CURLOPT_POST, true);
