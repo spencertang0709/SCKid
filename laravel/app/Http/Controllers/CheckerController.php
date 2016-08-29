14509e40076e72ead69960ca5c4a7897d47f767c
spencertang0709 <spencer.tang0709@gmail.com>
2016-08-29 20:56:30 +1200

    new

---
+++ laravel/app/Http/Controllers/CheckerController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Kid;
+use App\Post;
+use Illuminate\Http\Request;
+use App;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Session;
+
+class CheckerController extends Controller
+{
+    /**
+     * Show a list of all posts on child's facebook account
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $posts = NULL;
+        //This returns apps that the current child is using on there device
+        $kidID = Session::get('current_kid');
+        $currentKid = App\Kid::find($kidID);
+        if ($currentKid != NULL) {
+            $kid = $currentKid->socialMedias()->get()->first();
+            if($kid!=NULL) {
+                $posts = $kid->posts()->get();
+            }
+        }
+        return view('sensitive_checker', ['posts' => $posts]);
+    }
+}
